b0fe068ccbbd7209718b1e6bea134262942a162d
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-10 22:14:01 -0300

    carrers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from './pages/IndexPage';
+import Faq from './pages/Faq';
+import CarrersPage from './pages/Carrers';
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IndexPage />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/carreiras" element={<CarrersPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Faq/index.jsx
@@ -0,0 +1,16 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+function Faq() {
+    return (
+        <>
+            <Header />
+            <div className="container">
+                
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Faq;
