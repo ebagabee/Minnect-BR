9a6e2a40c8383b37bae12a702af59077ecd87b9a
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-11 22:05:15 -0300

    Finalizado

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,9 @@
 import './button.css';
+import React, { useState } from 'react';
 
 function Button({ background, color, width, height, content, weight, fontSize, border, display, radius }) {
+    const [isHovered, setIsHovered] = useState(false);
+
     const buttonStyle = {
         backgroundColor: background,
         color: color,
@@ -8,17 +11,27 @@ function Button({ background, color, width, height, content, weight, fontSize, b
         height: height,
         fontWeight: weight,
         fontSize: fontSize,
-        borderRadius: radius ||"8px",
+        borderRadius: radius || "8px",
         border: border,
         padding: "8px",
-        display: display || "block"
-    }
+        display: display || "block",
+        transition: "background-color 0.3s ease",
+        cursor: "pointer",
+    };
+
+    const buttonHoverStyle = {
+        backgroundColor: "lightgrey",
+    };
 
     return (
-        <button style={buttonStyle}>
+        <button
+            style={isHovered ? { ...buttonStyle, ...buttonHoverStyle } : buttonStyle}
+            onMouseOver={() => setIsHovered(true)}
+            onMouseOut={() => setIsHovered(false)}
+        >
             {content}
         </button>
-    )
+    );
 }
 
 export default Button;
